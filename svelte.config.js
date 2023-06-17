17333a1a04c3e93f37655e73855e3abd843fe36d
Diego Beltran <diego.beltrangg24@gmail.com>
2023-06-16 19:47:03 -0700

    se modifico el adaptador al de vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,13 +9,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true,
+            runtime: 'nodejs18.x',
         }),
         
 	},
