a132b4bc1cf6dd7290955466cfb5b064a4ae0c35
Diego Beltran <diego.beltrangg24@gmail.com>
2023-06-18 11:23:02 -0700

    se corrigio archivos para deploy

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/_layout.js';
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export { universal };
-export const universal_id = "src/routes/+layout.js";
-export const imports = ["_app/immutable/nodes/0.9406d5cd.js","_app/immutable/chunks/index.9f1f2401.js","_app/immutable/chunks/navigation.618fc51d.js","_app/immutable/chunks/singletons.f75a1827.js"];
-export const stylesheets = ["_app/immutable/assets/0.cd1a1567.css"];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/chunks/navigation.618fc51d.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.f75a1827.js";const e=o("goto");export{e as g};
