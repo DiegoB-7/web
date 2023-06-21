5cd11f0e30526cc55691245f7c4fe60ac21bd1dd
Diego Beltran <diego.beltrangg24@gmail.com>
2023-06-20 22:15:25 -0700

    se agrego responsive en header,footer y en sections de la homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script>
 	import Atropos from 'atropos';
 	import { onMount } from 'svelte';
-	
+
 	import img from '$lib/images/cristiano.png';
 	import { goto } from '$app/navigation';
-	
 
 	onMount(() => {
 		const myAtropos = Atropos({
@@ -18,32 +17,31 @@
 		});
 	});
 
-    function goAbout() {
+	function goAbout() {
 		goto('/about');
 	}
 </script>
 
 <main class="min-h-screen min-w-screen bg-black" style="overflow: hidden">
-	<section class="flex flex-row">
-		<div class="basis-1/2 flex items-center justify-center">
-			 <div class="flex flex-col">
-				<div>
-                    <div class="atropos my-atropos">
-                        <div class="atropos-scale">
-                            <div class="atropos-rotate">
-                                <div class="atropos-inner">
-                                    <img src={img} alt="profile" data-atropos-offset="5" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-			</div>
-            
-		</div>
-
-		<div class="basis-1/2 flex items-center">
+	<section class="flex flex-col md:flex-row mx-4 md:mx-0">
+		<div class="md:basis-1/2 flex items-center justify-center">
 			<div class="flex flex-col">
+				<div>
+					<div class="atropos my-atropos">
+						<div class="atropos-scale">
+							<div class="atropos-rotate">
+								<div class="atropos-inner">
+									<img src={img} alt="profile" data-atropos-offset="5" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<div class="md:basis-1/2 flex items-center">
+			<div class="flex flex-col mx-6">
 				<div class="my-1">
 					<p class="text-red font-heebo">Howdy, I'm Diego</p>
 				</div>
@@ -51,20 +49,133 @@
 					<h1 class="font-heebo text-white text-5xl font-bold">Fullstack Developer</h1>
 				</div>
 				<div class="my-1">
-                    <p class="text-lightgray font-heebo">Te ardes mucho</p>
-                </div>
+					<p class="text-lightgray font-heebo">
+						I'm a fullstack developer with a passion for building beautiful and functional websites.
+					</p>
+				</div>
 				<div class="my-1">
-					<button class="bg-red rounded-sm text-white hover:bg-gray delay-150 py-2 px-6 font-heebo" on:click={goAbout}>
+					<button
+						class="bg-red rounded-sm text-white hover:bg-gray delay-150 py-2 px-6 font-heebo"
+						on:click={goAbout}
+					>
 						About me
 					</button>
 				</div>
 			</div>
 		</div>
 	</section>
-     <section >
-        
+	
+	<section class="flex flex-col md:flex-row items-center justify-center mx-4 md:mx-40 my-20">
+		<div class="rounded overflow-hidden bg-gray mx-2 my-4 md:my-0  py-10 px-5 ">
+			<div class="flex-col ">
+				<div class="mx-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter fill-current text-red hover:text-lightgray h-8 w-8">
+						<path d="M3 6.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+  						<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v1H1V2a1 1 0 0 1 1-1h12zm1 3v10a1 1 0 0 1-1 1h-2V4h3zm-4 0v11H2a1 1 0 0 1-1-1V4h10z"/>
+					</svg>
+				</div>
+				<div class="my-4 mx-2">
+					<h4 class="font-heebo text-white text-xl font-bold">UI design</h4>
+				</div>
+				<div class="my-4 mx-2">
+					<p class="text-lightgray font-heebo break-words">
+						I value simple content structure, clean design patterns, and thoughtful interactions.
+					</p>
+				</div>
+				<div class="flex mx-2">
+					<a href="/" class="text-red font-heebo ">Know more </a>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter h-4 w-4 fill-current text-red hover:text-lightgray align-middle">
+						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<div class="rounded overflow-hidden bg-gray mx-2 my-4 md:my-0 py-10 px-5 ">
+			<div class="flex-col ">
+				<div class="mx-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter fill-current text-red hover:text-lightgray h-8 w-8">
+						<path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
+
+					</svg>
+				</div>
+				<div class="my-4 mx-2">
+					<h4 class="font-heebo text-white text-xl font-bold">Product Design</h4>
+				</div>
+				<div class="my-4 mx-2">
+					<p class="text-lightgray font-heebo break-words">
+						I like to code things from scratch, and enjoy bringing ideas to life in the browser.
+					</p>
+				</div>
+				<div class="flex mx-2">
+					<a href="/" class="text-red font-heebo ">Know more </a>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter h-4 w-4 fill-current text-red hover:text-lightgray align-middle">
+						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<div class="rounded overflow-hidden bg-gray mx-2 my-4 md:my-0 py-10 px-5 ">
+			<div class="flex-col ">
+				<div class="mx-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter fill-current text-red hover:text-lightgray h-8 w-8">
+						<path fill-rule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
+
+					</svg>
+				</div>
+				<div class="my-4 mx-2">
+					<h4 class="font-heebo text-white text-xl font-bold">Digital solutions consultancy</h4>
+				</div>
+				<div class="my-4 mx-2">
+					<p class="text-lightgray font-heebo break-words">
+						I enjoy helping people to create an amazing digital solution with the state-of-the-art technologies.
+					</p>
+				</div>
+				<div class="flex mx-2">
+					<a href="/" class="text-red font-heebo ">Know more </a>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+						class="hover:delay-150 bi bi-twitter h-4 w-4 fill-current text-red hover:text-lightgray align-middle">
+						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+					</svg>
+				</div>
+			</div>
+		</div>
+		
 	</section>
 </main>
 
-<style>
-</style>
+
--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,16 +1,15 @@
 <script>
     import { goto } from '$app/navigation';
-	
+	import img from '$lib/images/logo.png';
     import { onMount,onDestroy } from 'svelte';
 
+    let element;
+	let isVisible = false;
     onMount(() => {
-        const element = document.querySelector('.header');
-        element.classList.add('animate__animated');
-        element.classList.add('animate__fadeInDownBig');
-        element.style.setProperty('--animate-duration', '1s');
-    });
+    	const observer = new IntersectionObserver(handleIntersection);
+    	observer.observe(element);
+  	});
 
-    
     function goHome() {
 		goto('/');
 	}
@@ -21,31 +20,67 @@
 		goto('/contact');
 	}
     
+	function onShow(){
+        const element = document.querySelector('.header');
+        element.classList.add('animate__animated');
+        element.classList.add('animate__fadeInDownBig');
+        element.style.setProperty('--animate-duration', '1s');
+		setTimeout(() => {
+			onExit();
+		}, 1000);
+		
+    }
+    function onExit(){
+        const element = document.querySelector('.header');
+        element.classList.remove('animate__animated');
+        element.classList.remove('animate__fadeInDownBig');
+    }
+
+	function handleIntersection(entries) {
+   		entries.forEach(entry => {
+  			isVisible = entry.isIntersecting;
+			console.log(isVisible);
+  			if (isVisible) {
+    		  onShow();
+			  
+    		}
+		});
+  }
+
+  	function toggleMenu() {
+        const menu = document.getElementById("menu");
+        menu.classList.toggle("hidden");
+    }
 </script>
 
-<header class=" top-0 w-full  header relative " style="overflow: hidden">
-<div class="flex flex-row mx-10 my-5">
-    <div class="flex justify-start basis-1/2">
-
-    </div>
-    <div class="flex justify-end basis-1/2">
-        <div class="basis-1/3">
-            <button on:click={goHome}  >
-                <p class="text-white hover:text-lightgray hover:delay-150 mx-3 font-heebo links" >
-                    Home
-                </p></button
-            >
-            <button on:click={goAbout}>
-                <p class="text-white hover:text-lightgray hover:delay-150 mx-3 font-heebo ">
-                    About
-                </p></button
-            >
-            <button on:click={goContact}>
-                <p class="text-white hover:text-lightgray hover:delay-150 mx-3  font-heebo ">
-                    Contact
-                </p></button
-            >
+<header class="top-0 w-full header relative" style="overflow: hidden" bind:this={element} >
+    <div class="flex flex-col md:flex-row mx-10 md:mx-0 ">
+        <div class="flex justify-between items-center md:mx-40">
+            <img src={img} alt="logo" class="w-24 h-24 object-cover">
+            <button on:click={toggleMenu} class="md:hidden flex items-center px-3 py-2 border rounded text-white border-white hover:text-lightgray hover:border-lightgray">
+				<svg class="h-4 w-4 fill-current text-white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0 3h20v2H0zm0 5h20v2H0zm0 5h20v2H0z" />
+				</svg>
+			</button>
         </div>
+        <div class="hidden md:flex right-0   justify-end items-center md:items-stretch md:space-x-4 flex-auto md:mx-40" id="menu">
+			<button on:click={goHome}>
+				<p class="text-white hover:text-lightgray hover:delay-150 mx-3 font-heebo links">Home</p>
+			</button>
+			<button on:click={goAbout}>
+				<p class="text-white hover:text-lightgray hover:delay-150 mx-3 font-heebo">About</p>
+			</button>
+			<button on:click={goContact}>
+				<p class="text-white hover:text-lightgray hover:delay-150 mx-3 font-heebo">Contact</p>
+			</button>
+		</div>
     </div>
-</div>
 </header>
+
+<style>
+	@media (max-width: 767px) {
+        .hidden {
+            display: none;
+        }
+    }
+</style>
